da04d6469789c4014c8cb86924b7a9b63b90b76f
Sravani Kunapuli <ckt3812@autuni.ac.nz>
2026-04-09 20:39:48 +1200

    added intial shipment class just the structutre and i wrote don the methods for it from the class diagram

---
+++ Shipment.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace MainfreightProject;
+{
+
+    public class Shipment
+    {
+
+        private string shipmentID;
+        private string status;
+        private string currentLocation;
+        private string deliveryStatus;
+
+        public Shipment(string shipmentID,string status , string currentLocation, string deliveryStatus)
+        {
+        
+        }
+
+        public void TrackShipment()
+        {
+        
+        }
+
+        public void UpdateStatus(string newStatus)
+        {
+        
+        }
+
+        public string getShipmentInfo()
+        {
+            return "";
+        }
+    
+    }
+
+}
--- User.cs
+++ User.cs
@@ -2,19 +2,22 @@ using System;
 
 namespace MainfreightProject
 {
+    //this is the user class whihc is used to stotre common user details, for all the different users in the mainfreihgt system
     public class User
     {
+        // i have used private to make sure that external perosnalle cannot enter or view user informaiton, for privacu reasons
         private string userID;
         private string name;
         private string email;
 
+        //this will intialise the user object
         public User(string userID, string name, string email)
         {
         }
 
         public string ViewProfile()
         {
-            return "";
+            return "";// i am using return so that when there is an object it can be viewed , nd whatver the object is inside it will be returned and viewable
         }
     }
 }
